856b996d460d3ae3baf9f02dcee52a675b4b7d5e
Ville Kosola <ville.kosola@yahoo.com>
2019-04-17 15:43:54 +0300

    first layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridListComponent } from './grid-list/grid-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridListComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
