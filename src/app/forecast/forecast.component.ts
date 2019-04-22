4c916e838c64ca8b7a9e1dae3d920a79794a6376
Ville Kosola <ville.kosola@yahoo.com>
2019-04-22 16:36:53 +0300

    builded production v

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -10,6 +10,21 @@ export class ForecastComponent implements OnInit {
 
   constructor() { }
 
+  // public getPosts(): void {
+  //   this.dataService.getCurrent(this.city).subscribe((res: any) => {
+  //     this.Root = res;
+  //     this.Cloud = res.clouds;
+  //     this.Coords = res.coord;
+  //     this.weather = res.weather[0];
+  //     this.winds = res.wind;
+  //     this.mains = res.main;
+  //     this.system = res.sys;
+  //     this.temperature=Math.round(this.mains.temp-272);
+  //     this.loaded=true;
+  //     console.log("getPosts called with city: " + this.city)
+  //   });  
+  // }
+
   ngOnInit() {
     
   }
